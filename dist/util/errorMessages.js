c792002ed37098d56d6fc13ebae50fed3d0e0975
Aravind KS <aravindks@Aravinds-MacBook-Air.local>
2022-08-05 22:40:12 +0530

    remove dist from repo

--- dist/util/errorMessages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorMessages = void 0;
-var ErrorMessages;
-(function (ErrorMessages) {
-    ErrorMessages["INVALIDPARAM"] = "Please enter valid parameters";
-})(ErrorMessages = exports.ErrorMessages || (exports.ErrorMessages = {}));
